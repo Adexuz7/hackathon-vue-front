9c989209832eec63deb3fb13714b8e5d03f15909
Andrés Galván <adexuz7@gmail.com>
2021-07-01 12:28:09 +0100

    comments is now a component

---
+++ src/components/Comments.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card class="mx-auto" max-width="400" tile>
+    <v-list-item three-line v-for="(comment, index) in comments" :key="index">
+      <v-list-item-content>
+        <v-list-item-title>{{ comment.author }}</v-list-item-title>
+        <v-list-item-subtitle>
+          {{ comment.comment }}
+        </v-list-item-subtitle>
+        <v-list-item-subtitle>
+          {{ comment.rate }}
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    comments: Array,
+  },
+};
+</script>
